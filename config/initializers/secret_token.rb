c0020ca7bd1453705274f8e90c3d76689024827f
Shana Weitzen <shanaweitzen@gmail.com>
2013-07-14 12:49:21 -0700

    create project2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Project2::Application.config.secret_key_base = '4467683d7cab1fc83cf5a8daf492a1037cb210896861dd2821fce4b28bc7612cbe79a696c5202fd3db31edba1d699d2e6be5cfffa44650a7302872477a5d8b00'
---
+++ vendor/bundle/gems/atomic-1.1.10/ext/siteconf20130714-1214-yq23z9.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130714-1214-1h09k6n"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
