455dad74d6e9f4aa8cd15622d36dd1e2116acee9
Erin <zazainca@gmail.com>
2013-07-20 15:28:47 -0700

    Updating code

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Project2::Application.routes.draw do
   get "terms" => "site#terms"
   
   resource :session , only: [ :new, :create, :destroy ]
-  
+  resource :password, only: [ :edit, :update ]
+
   get "login" => "session#new"
   post "login" => "session#create"
   delete "logout" => "session#destroy"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+
+User.create([
+	{
+		email: "joe@mama.com",
+		password: "",
+		password_confirmation: "12345"
+	},
+	{
+		email: "jane@papa.com",
+		password: "12345",
+		password_confirmation: "12345" 
+	},
+	{
+		email: "timmy@baby.com",
+		password: "12345",
+		password_confirmation: "12345"
+		}
+])
